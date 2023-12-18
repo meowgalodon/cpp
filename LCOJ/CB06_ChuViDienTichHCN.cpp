07666266a907482f9764106b644a5a1b644d226b
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2023-12-18 12:52:44 +0700

    Add files via upload

---
+++ LCOJ/CB06_ChuViDienTichHCN.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	unsigned int a, b;
+	cin >> a >> b;
+	cout << (a + b) * 2 << endl;
+	cout << a * b;
+	return 0;
+}
---
+++ LCOJ/CB07_ChuViDienTichHinhTron.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	float r;
+	cin >> r;
+	cout << fixed << setprecision(3) << 2 * 3.14 * r << " ";
+	cout << fixed << setprecision(3) << 3.14 * r * r;
+	return 0;
+}
