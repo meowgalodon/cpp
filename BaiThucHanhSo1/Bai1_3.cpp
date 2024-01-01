97e7dcf7fa0a11a9374907ff21b3a91f8c49b94d
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2024-01-01 21:41:54 +0700

    Add files via upload

---
+++ BaiThucHanhSo1/Bai1_3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Tinh + - * / cho 2 so nguyen bat ky
+int main(){
+	
+	//Khai bao 2 so nguyen
+	float a, b;
+	cin >> a >> b;
+	
+	//Phep +
+	cout << a << " + " << b << " = " << a + b << endl;
+	
+	//Phep -
+	cout << a << " - " << b << " = " << a - b << endl;
+	
+	//Phep *
+	cout << a << " * " << b << " = " << a * b << endl;
+	
+	//Phep /
+	cout << a << " / " << b << " = " << a / b << endl;
+	return 0;
+}
