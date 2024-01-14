cd387fee05e1b3e33d1565e6bc1e94fc25a17f2c
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2024-01-14 12:50:58 +0700

    Add files via upload

---
+++ OOP/TestFor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(){
+//	int n = 10;
+//	int m = 9;
+	
+	//Bang cuu chuong     
+	for(int j = 1; j <= 10; j++){
+		for(int i = 2; i <= 9; i++){
+			std::cout << "\t" << i << " x " << j << " = "<< i * j << "   ";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
