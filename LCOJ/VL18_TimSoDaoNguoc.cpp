8d7b4956ce3f5b16766f2ae731ef8bb539973336
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2024-01-22 10:54:26 +0700

    Update VL18_TimSoDaoNguoc.cpp

--- LCOJ/VL18_TimSoDaoNguoc.cpp
+++ LCOJ/VL18_TimSoDaoNguoc.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 #include <string.h>
 
-// Tim so dao nguoc
-// Vi du: 1234 --> 4321
-// Y tuong: 1234 % 10 = 4 * 10 + 3 = 43 * 10 + 2 = 432 * 10 + 1 = 4321
-//		->	123	 % 10 = 3
-//		-> 	12	 % 10 = 2
-// 		-> 	1	 % 10 = 1
-//		-> 	0 la dieu kien dung 
-
-
 int main()
 {
 	std::string str;
