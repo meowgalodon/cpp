8038ccee3e4044dad2f360600d8c3485e0831ef7
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2024-03-08 10:16:14 +0700

    Add files via upload

---
+++ LCOJ/DEMTU_BeHocTiengAnh.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+// Dem tu
+
+using namespace std;
+
+string s;
+
+void solve()
+{
+	int count = 0;
+	while(cin >> s)
+	{
+		count++;
+	}
+	cout << count;
+}
+
+int main()
+{
+	solve();
+	return 0;
+}
