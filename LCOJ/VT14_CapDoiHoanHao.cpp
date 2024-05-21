40209e9727548096cfe379f01b1fdd15fbb1f23e
Duy Mai <142564761+meowgalodon@users.noreply.github.com>
2024-05-21 21:51:58 +0700

    Add files via upload

---
+++ LCOJ/VT14_CapDoiHoanHao.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a , a + n );
+	cout << max(a[0] * a[1], a[n - 2] * a[n - 1]);
+	return 0;
+}
