91a5679d392f4ed9a622dd58f79b65236576d212
Duy Mai <142564761+meowgalodon@users.noreply.github.com>
2024-05-25 23:32:03 +0700

    Update full AC

--- LCOJ/MT02_TinhTongCacHangCoChiSoLe.cpp
+++ LCOJ/MT02_TinhTongCacHangCoChiSoLe.cpp
@@ -2,69 +2,29 @@
 
 using namespace std;
 
-void input(vector<vector<int>>& maTran)
-{
-	int m;
-	cin >> m;
-	
-	int n;
-	cin >> n;
-	
-	maTran.resize(m, vector<int>(n));
-	
-	for(int i = 0; i < m; i++)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			cin >> maTran[i][j];
-		}
-	}
-}
-
-// Ham check hang chi co duy nhat mot so le
-bool check(const vector<int>& m)
-{
-	int count = 0;
-	for(int num : m)
-	{
-		if(num % 2 != 0)
-		{
-			++count;
-		}
-	}
-	return count == 1;
-}
-
-// Ham tinh tong cac so hang tren hang chi co duy nhat mot so le
-int solve(const vector<vector<int>>& maTran)
-{
-	int sum = 0;
-	for(const auto& hang : maTran)
-	{
-		if(check(hang))
-		{
-			for(int num : hang)
-			{
-				sum += num;
-			}
-		}
-	}
-	return sum;
-}
-
 int main()
 {
-	vector<vector<int>> maTran;
-	input(maTran);
-	
-	for(const auto& hang : maTran)
+    int m, n;
+    cin >> m;
+    cin >> n;
+
+    int A[100][100]; 
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    int tong = 0;
+    for (int i = 1; i < m; i += 2)
+    { 
+        for (int j = 0; j < n; ++j)
 	{
-		int tong = solve(maTran);
-		if(tong > 0)
-		{
-			cout << tong;
-			break;
-		}
-	}
-	return 0;
+            tong += A[i][j];
+        }
+    }
+
+    cout << tong;
+return 0;
 }
