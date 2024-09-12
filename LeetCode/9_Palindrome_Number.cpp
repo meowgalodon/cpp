e2075d5a300e037acbeeb05a31183ca4ce314e80
Duy Mai <142564761+meowgalodon@users.noreply.github.com>
2024-09-12 23:40:05 +0700

    Add files via upload

---
+++ LeetCode/9_Palindrome_Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;  
+        long reversed = 0;  
+
+        while (x > 0) {
+            int digit = x % 10;  
+            reversed = reversed * 10 + digit;  
+            x /= 10; 
+        }
+
+        return original == reversed;
+    }
+};
+
