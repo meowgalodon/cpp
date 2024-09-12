9475a79dd2de4a2c3075d15f651cbabff2f505e2
Duy Mai <142564761+meowgalodon@users.noreply.github.com>
2024-09-12 23:44:02 +0700

    Add files via upload

---
+++ LeetCode/13_Roman_to_Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        // create a map to store value
+        std::unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int total = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && roman[s[i]] < roman[s[i + 1]]) {
+                total -= roman[s[i]];
+            } else {
+                total += roman[s[i]];
+            }
+        }
+        
+        return total;
+    }
+};
